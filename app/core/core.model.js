56bddab82a3938725d7a12189e0ec2f16dc73c14
puguhjayadi <puguh.jayadi@gmail.com>
2020-05-26 19:37:32 +0700

    add core model

---
+++ app/core/core.model.js
@@ -0,0 +1,91 @@
+const sql = require("../models/db.js");
+
+module.exports = function(table) {
+
+  return {
+    getAll : function(result) {
+      sql.query("SELECT * FROM "+table, (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+        result(null, res);
+      });
+    },
+
+    findById : function(id, result) {
+      sql.query("SELECT * FROM "+table+" WHERE id = ?", [id], (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+        if (res.length) {
+          console.log("found object: ", res[0]);
+          result(null, res[0]);
+          return;
+        }
+
+        result(null, { message: "not found" });
+
+      });
+    },
+
+    create : function (object, result) {
+      sql.query("INSERT INTO "+table+" SET ?", object, (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+
+        console.log("created object: ", { id: res.insertId, ...object });
+        result(null, { id: res.insertId, ...object });
+      });
+    },
+
+    updateById : function (id, object, result) {
+      sql.query( "UPDATE "+table+" SET ? WHERE id = ?", [object, id], (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(null, err);
+          return;
+        }
+
+        if (res.affectedRows == 0) {
+          result(null, { message: "not found" });
+          return;
+        }
+
+        console.log("updated object: ", { id: id, ...object });
+        result(null, { id: id, ...object });
+      }
+      );
+    },
+
+    remove : function(id, result) {
+      sql.query("DELETE FROM "+table+" WHERE id = ?", id, (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(null, err);
+          return;
+        }
+
+        if (res.affectedRows == 0) {
+          result(null, { message: "not found" });
+          return;
+        }
+
+        console.log("deleted object with id: ", id);
+        result(null, res);
+      });
+    },
+
+
+  }
+};
+
+
+
+
